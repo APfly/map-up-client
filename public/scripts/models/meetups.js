17082b395614d4919439d9aa06446e3ca96b0ec1
EmeryP <emeryparks09@hotmail.com>
2018-05-01 16:55:59 -0700

    search map by input functioning properly

--- public/scripts/models/meetups.js
+++ public/scripts/models/meetups.js
@@ -9,3 +9,21 @@ ENV.isProduction = window.location.protocol === 'https:';
 ENV.productionApiUrl = 'https://apfly-map-up.herokuapp.com/';
 ENV.developmentApiUrl = 'http://localhost:3000';
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
+
+// wrapping all functions in an IIFE
+(function(module){
+  function errorCallback(err){
+    console.error(err);
+    module.errorView.initErrorPage(err);
+  }
+  // Defining Meetups constructor
+  function Meetups(rawMeetupsObj){
+    Object.keys(rawMeetupsObj).forEach(key => this[key] = rawMeetupsObj[key])
+  }
+  // creating handlebars template with Meetups data
+  Meetups.prototype.toHtml = function(){
+    let template = Handlebars.complile($('#meetups-template').text());
+    return template(this);
+  }
+
+})(app);

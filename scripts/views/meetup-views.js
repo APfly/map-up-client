d40063ed4eaa79c04d0e255be3c7d4098c36bffe
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-05-03 13:56:53 -0700

    updating misc files

--- scripts/views/meetup-views.js
+++ scripts/views/meetup-views.js
@@ -19,8 +19,29 @@ var app = app || {};
     $('.container').fadeIn(150);
     $('#table ul').empty();
 
-    app.Meetups.all.forEach(item => $('#table ul').append(item.toHtml()));
+    app.Meetups.all.forEach(item => $('#table ul').append(item.toHtml(), `<button class="save-meetup">save to my meetups</button>`));
+
+    let saveMeetup = document.getElementsByClassName('save-meetup');
+    for (let i = 0; i < saveMeetup.length; i++) {
+      console.log('adding listener');
+      saveMeetup[i].addEventListener('click', saveToMyMeetups);
+    }
+  }
+
+  meetupView.initMyMeetupsPage = function (ctx, next) {
+    resetView();
+    $('.container').fadeIn(150);
+    $('#my-meetups ul').empty();
+    app.Meetups.saved.forEach(item => $('#my-meetyps ul').append(item.toHtml()));
+  }
+
+  function saveToMyMeetups(event) {
+    event.preventDefault();
+    // console.log($(this).prev());
+    console.log('saving the following:', $(this).prev());
+    app.Meetups.saved.push(app.Meetups.all[$(this).prev().index() / 2]);
   }
   module.meetupView = meetupView;
 })(app)
+
 

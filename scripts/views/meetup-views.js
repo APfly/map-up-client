d66f35b4dd4b53fafe583ee0eb5e5f9e8ebd686b
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-05-07 01:37:11 -0800

    last minute UX changes, refactored adding event listener and deleting event listener, added button to Handlebars template by default

--- scripts/views/meetup-views.js
+++ scripts/views/meetup-views.js
@@ -12,40 +12,44 @@ var app = app || {};
     $('#location-input').val('');
     $('#table ul').empty();
     $('#my-meetups ul').empty();
-    app.Meetups.all.forEach(item => $('#table ul').append(item.toHtml(), `<button class="save-meetup">save to my meetups</button>`));
+    app.Meetups.all.forEach(item => $('#table ul').append(item.toHtml()));
 
-    let saveMeetup = document.getElementsByClassName('save-meetup');
-    for (let i = 0; i < saveMeetup.length; i++) {
-      console.log('adding listener');
-      saveMeetup[i].addEventListener('click', saveToMyMeetups);
-    }
+    $('.save-meetup').click(function saveToMyMeetups(event) {
+      event.preventDefault();
+      if (app.Meetups.saved.some(meetup => meetup.link === app.Meetups.all[$(this).closest('li').index()].link)) {
+        console.log('you already have that meetup');
+      }
+      else {
+        app.Meetups.saved.push(app.Meetups.all[$(this).closest('li').index()]);
+        console.log('unique entry added:', app.Meetups.all[$(this).closest('li').index()].name);
+      }
+      var saveMeetups = JSON.stringify(app.Meetups.saved);
+      localStorage.setItem('saved-meetups', saveMeetups);
+      $(this).hide();
+      $(this).closest('li').append(`<p class="added-note">added to my meetups &#10003</p>`);
+    });
   }
 
   meetupView.initMyMeetupsPage = function (ctx, next) {
-
     disMyMeetups();
     $('#table ul').empty();
     $('#myMeetUps ul').empty();
     app.Meetups.saved.forEach(item => $('#myMeetUps ul').append(item.toHtml()));
+    $('.save-meetup').addClass('delete-meetup').removeClass('save-meetup');
+    $('.delete-meetup').text('delete from my meetups');
+
+    $('.delete-meetup').click(function saveToMyMeetups(event) {
+      event.preventDefault();
+      console.log($(this).closest('li').index());
+      let deleteIndex = $(this).closest('li').index();
+      console.log('deleting the following Meetup:', app.Meetups.saved[$(this).closest('li').index()].name);
+      app.Meetups.saved = $.grep(app.Meetups.saved, function (value) {
+        return value != app.Meetups.saved[deleteIndex];
+      });
+      console.log(app.Meetups.saved);
+      $(this).closest('li').remove();
+    });
   }
-
-  function saveToMyMeetups(event) {
-    event.preventDefault();
-    console.log('saving the following:', $(this).prev())
-    console.log(app.Meetups.all[$(this).prev().index() / 2].link);
-    if (app.Meetups.saved.some(meetup => meetup.link === app.Meetups.all[$(this).prev().index() / 2].link)) {
-      console.log('you already have that meetup');
-    }
-    else {
-      app.Meetups.saved.push(app.Meetups.all[$(this).prev().index() / 2]);
-      console.log('unique entry added');
-    }
-    var saveMeetups = JSON.stringify(app.Meetups.saved);
-    localStorage.setItem('saved-meetups', saveMeetups);
-    $(this).hide();
-  }
-
-
   module.meetupView = meetupView;
 })(app)
 
